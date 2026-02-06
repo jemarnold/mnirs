e17e633644492eefac3f270e9b94eaa57bea64f9
JEMSDESKTOP <jem.arnold@gmail.com>
2026-02-05 22:07:06 -0800

    update example data files

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' @name artinis_intervals.xlsx
 #'
-#' @title 10 Hz Artinis Oxysoft export
+#' @title 10 Hz Artinis Oxysoft export recorded with Oxymon MKIII
 #'
 #' @description Exported from Artinis Oxysoft, recorded on Oxymon MKIII at
 #'   50 Hz and exported at 10 Hz. Containing two 5-minute cycling work
@@ -9,11 +9,11 @@
 #'
 #' @docType data
 #'
-#' @format .xlsx file with header metadata and with four columns and 20919 rows.
+#' @format .xlsx file with header metadata and with five columns and 20919 rows.
 #'
 #'   - `nirs_channels = c(O2Hb = 2, HHb = 3)`
 #'   - `time_channel = c(sample = 1)`
-#'   - `event_channel = c(event = 4)`
+#'   - `event_channel = c(event = 4, label = "col_5")`
 #'
 NULL
 
@@ -64,13 +64,13 @@ NULL
 #'
 #' @description Exported from Train.Red app, recorded at 10 Hz. Containing
 #'   two 5-minute cycling work intervals, placed on bilateral vastus lateralis
-#'   muscle sites.
+#'   muscle sites. Some data channels have been omitted to keep file size down.
 #'
 #' @docType data
 #'
-#' @format .csv file with header metadata and with 18 columns and 12001 rows.
+#' @format .csv file with header metadata and with 10 eolumns and 12001 rows.
 #'
-#'   - `nirs_channels = c("SmO2", "SmO2 unfiltered", "HBDiff", "O2HB unfiltered", "HHb unfiltered", "THb unfiltered")`
+#'   - `nirs_channels = c("SmO2 unfiltered", "HBDiff unfiltered")`
 #'   - `time_channel = c("Timestamp (seconds passed)")`
 #'   - `event_channel = c("Lap/Event")`
 #'   - `event_times = c(2455, 3166)` ## c(371, 1082) from zero_time
@@ -91,5 +91,24 @@ NULL
 #'
 #'   - `nirs_channels = c("SmO2[%]", "SmO2 -  2[%]", "SmO2 -  3[%]", "THb[THb]", "THb -  2[THb]", "THb -  3[THb]")`
 #'   - `time_channel = c("Time[s]", "Time[hh:mm:ss]")`
+#'
+NULL
+
+
+#' @name portamon-oxcap.xlsx
+#'
+#' @title 10 Hz Artinis Oxysoft export recorded with Portamon
+#'
+#' @description Exported from Artinis Oxysoft, recorded on Portamon at
+#'   10 Hz on the vastus lateralis muscle. Containing two trials of repeated 
+#'   occlusion oxidative capacity testing, each with 17 occlusions.
+#'
+#' @docType data
+#'
+#' @format .xlsx file with header metadata and with six columns and 7943 rows.
+#'
+#'   - `nirs_channels = c(THb = 2, HHb = 3, O2Hb = 4)`
+#'   - `time_channel = c(sample = 1)`
+#'   - `event_channel = c(event = 4, label = "col_6")`
 #'
 NULL
