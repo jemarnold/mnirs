08f575dd686275a27701868dc60ebdf272d3ce58
JEMSDESKTOP <jem.arnold@gmail.com>
2026-03-07 22:20:53 -0800

    update `train.red` times

--- R/data.R
+++ R/data.R
@@ -73,8 +73,8 @@ NULL
 #'   - `nirs_channels = c("SmO2", "SmO2 unfiltered", "O2HB unfiltered", "HHB unfiltered")`
 #'   - `time_channel = c("Timestamp (seconds passed)")`
 #'   - `event_channel = c("Lap/Event")`
-#'   - `interval_times = list(start = c(2150.09, 2865.05), end = c(2457.05, 3168.08))` 
-#'   - `interval_times = list(start = c(65.94, 780.90), end = c(372.90, 1183.93))` ## from zero_time
+#'   - `interval_times = list(start = c(2150.09, 2865.05), end = c(2441.06, 3168.08))` 
+#'   - `interval_times = list(start = c(65.94, 780.90), end = c(356.91, 1183.93))` ## from zero_time
 #'
 NULL
 
